3b5d9c0ea83cd790b73b264b36843a51145aa4cc
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-07-19 22:47:20 -0500

    updated code

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
 console.log(profileDataArgs);
 
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
+console.log(name, github);
 
-    // This...
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-
-        console.log(profileDataArr[i]);
-
-    }
-
-    console.log('================');
-
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const pageHTML = generatePage(name, github);
 
 
-    
-    
-};
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
