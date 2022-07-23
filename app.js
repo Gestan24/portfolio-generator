95afa0b12061b52b4dd3b55bfbf8eff25ace3534
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-07-23 00:41:59 -0500

    generate entire website

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const inquirer = require('inquirer');
 
@@ -218,13 +218,32 @@ promptUser()
   .then(portfolioData => {
 
 
-    const pageHTML = generatePage(portfolioData);
+    return generatePage(portfolioData);
 
+  })
 
-    fs.writeFile('index.html', pageHTML, err => {
-      if (err) throw err;
+  .then(pageHTML => {
 
-      console.log('Portfolio complete! Check out index.html to see the output!');
-    });
+    return writeFile(pageHTML);
+
+  })
+
+  .then(writeFileResponse => {
+
+    console.log(writeFileResponse);
+
+    return copyFile();
+
+  })
+
+  .then(copyFileResponse => {
+
+    console.log(copyFileResponse);
+
+  })
+
+  .catch(err => {
+
+    console.log(err);
 
   });
